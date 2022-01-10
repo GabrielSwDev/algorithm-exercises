4be40a5f0c777b2e66e0bf8bb7c8f8ed9bf4aaaf
=Gabriel Jose <gabrieljosee@gmail.com>
2022-01-10 09:12:09 -0500

    Initial Commit: - 2 basic entry level algorithm exercises.

---
+++ Algorithm.Entry/Src/ArrayRotationTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Algorithm.Entry.Src
+{
+    public class ArrayRotationTest
+    {
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4, new int[] { 5, 1, 2, 3, 4 })]
+        public void Rotate_Array(int[] array, int rotationQty, int[] expected)
+        {
+            var numCollection = array.ToList();
+
+            while (rotationQty > 0)
+            {
+                var itemToRemoved = numCollection[0];
+                numCollection.Remove(itemToRemoved);
+
+                numCollection.Add(itemToRemoved);
+                rotationQty--;
+            }
+
+
+
+
+            Assert.Equal(expected, numCollection);
+        }
+
+
+    }
+}
---
+++ Algorithm.Entry/Src/InterchangeStringTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using Xunit;
+
+namespace Algorithm.Entry.Src
+{
+    public class InterchangeStringTest
+    {
+        [Theory]
+        [InlineData("abc", "pqr", "apbqcr")]
+        [InlineData("ab", "pqrs", "apbqrs")]
+        [InlineData("abcd", "pq", "apbqcd")]
+        public void Interchange_Two_Words(string word1, string word2, string expected)
+        {
+            var result = InterchangeWord.Execute(word1, word2);
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public static class InterchangeWord
+    {
+        public static string Execute(string word1, string word2)
+        {
+            var biggestLength = Math.Max(word1.Length, word2.Length);
+            var newWordBuilder = new StringBuilder();
+
+            void AddLetterToTheBuilder(char? letter)
+            {
+                if (letter != null)
+                    newWordBuilder.Append(letter);
+            }
+
+            for (int i = 0; i < biggestLength; i++)
+            {
+                AddLetterToTheBuilder(word1.GetLetter(i));
+                AddLetterToTheBuilder(word2.GetLetter(i));
+
+            }
+
+            return newWordBuilder.ToString();
+        }
+
+        public static char? GetLetter(this string word, int index)
+        {
+            if ((index + 1) > word.Length)
+                return null;
+
+            return word[index];
+        }
+    }
+}
