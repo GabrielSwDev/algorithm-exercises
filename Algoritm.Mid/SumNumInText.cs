90cb7c2a50dcb51e052f6e5ce85587ff63f47a28
Gabriel Hernandez <it.gabrieljose@gmail.com>
2022-01-27 09:53:10 -0500

    - added the first mid level exercise SumNumInText

---
+++ Algoritm.Mid/SumNumInText.cs
@@ -0,0 +1,25 @@
+using Xunit;
+using System.Linq;
+
+namespace Algoritm.Mid
+{
+    public class SumNumInText
+    {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("2,8, 1,3", 14)]
+        [InlineData("1,7, ab,", 8)]
+        public void Execute(string text, int expected)
+        {
+            var result = text.Trim().Split(",").Sum(n => ToNumber(n));
+
+            int ToNumber(string str)
+            {
+                int.TryParse(str, out int castedNumber);
+                return castedNumber;
+            }
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
