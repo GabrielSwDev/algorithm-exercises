5c8193d9db3f173f858f23b5b5ff85c45ebf2f5d
Gabriel Hernandez <it.gabrieljose@gmail.com>
2022-11-19 16:03:37 -0500

    Added DNA Sequences exercise, resolved using the sliding window pattern big = O(n)
    added fluentassertions library

---
+++ Algoritm.Mid/Src/DNASequences.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Algoritm.Mid.Src
+{
+
+    public class DNASequences
+    {
+        [Theory]
+        [InlineData("AAAAACCCCCAAAAACCCCCC", 8, new string[] { "AAAACCCC", "AAAAACCC", "AAACCCCC" })]
+        [InlineData("GGGGGGGGGGGGGGGGGGGGGGGGG", 12, new string[] { "GGGGGGGGGGGG" })]
+        [InlineData("", 10, new string[] {})]
+        public void Execute(string input, int k, string[] expected)
+        {
+            var result = new HashSet<string>();
+            if (string.IsNullOrEmpty(input))
+            {
+                goto Assert;
+            }
+               
+
+            var uniqueList = new HashSet<string>();
+            var windowBuilder = new StringBuilder();
+
+            //iterate & build the first window
+            for (int i = 0; i < k; i++)
+            {
+                windowBuilder.Append(input[i]);
+            }
+
+            uniqueList.Add(windowBuilder.ToString());
+
+            /*to build the remaining windows
+             * remove the first char from previous window and add the nth char to the new one.
+             */
+
+            for (int i = k; i < input.Length; i++)
+            {
+                windowBuilder.Remove(0, 1);
+                windowBuilder.Append(input[i]);
+
+                if (!uniqueList.Contains(windowBuilder.ToString()))
+                    uniqueList.Add(windowBuilder.ToString());
+                else if(!result.Contains(windowBuilder.ToString()))
+                    result.Add(windowBuilder.ToString());
+            }
+
+            Assert:
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
--- Algoritm.Mid/Src/SumNumInText.cs
+++ Algoritm.Mid/Src/SumNumInText.cs
@@ -1,7 +1,7 @@
 using Xunit;
 using System.Linq;
 
-namespace Algoritm.Mid
+namespace Algoritm.Mid.Src
 {
     public class SumNumInText
     {
